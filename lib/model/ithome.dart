8fe8ba8f954dcb651444a6b3ed42b0a36a68752e
nmk0718 <nimingkun@liangjian.com>
2022-03-16 23:13:20 +0800

    Create ithome.dart

---
+++ lib/model/ithome.dart
@@ -0,0 +1,18 @@
+class ithome {
+  String title;
+  String link;
+
+  ithome({this.title, this.link});
+
+  ithome.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    link = json['link'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['link'] = this.link;
+    return data;
+  }
+}
